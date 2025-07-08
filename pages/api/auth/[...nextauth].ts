49c1d5df5e6187aba22911e5ef299810a3c4edfe
makoto715 <makoto715@hotmail.co.jp>
2025-07-08 17:30:11 +0900

    Fix: remove unused type imports causing ESLint error

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,8 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import "next-auth/jwt";
-import { Session } from "next-auth";
-import { JWT } from "next-auth/jwt";
 
 declare module "next-auth/jwt" {
   interface JWT {
